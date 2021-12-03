c6f01d5155c4268d9b6868b8d85d5e5760a6ab0a
Daniel Christo <daniel.c@thinkific.com>
2021-12-03 12:27:26 -0500

    Refactored a bit more

--- day3/challenge_2.py
+++ day3/challenge_2.py
@@ -1,41 +1,27 @@
 #!/usr/bin/python
 
-def most_common_bit_at_index(list, bit_index):
-    indicator = 0
+def most_and_least_common_bit_lists(list, bit_index):
+    ones = []; zeros = []
+    
     for item in list:
-        if item[bit_index] == '0':
-            indicator-=1
-        else:
-            indicator+=1
-    return indicator >= 0
+        if item[bit_index] == '0': zeros.append(item)
+        else: ones.append(item)
+    if len(ones) >= len(zeros): return {'most common': ones, 'least common': zeros}
+    else: return {'most common': zeros, 'least common': ones}
 
-def filter_by_bit(list, bit_val, bit_index):
-    result_list = []
-    str_bit_val = str(int(bit_val))
-    for item in list:
-        if item[bit_index] == str_bit_val:
-            result_list.append(item)
-    return result_list
-    
-with open('input.txt') as f:
-    lines = f.read().splitlines() 
+def life_support_rating(input):
+    lists = most_and_least_common_bit_lists(input, 0)
+    oxy_gen_rating = lists['most common']
+    co2_scrub_rating = lists['least common']
 
-oxy_list = lines
-co_list = lines
-for i in range(0, 12):
-    most_common_bit = most_common_bit_at_index(oxy_list, i)
-    least_common_bit = not most_common_bit_at_index(co_list, i)
-    if len(oxy_list) > 1:
-        oxy_list = filter_by_bit(oxy_list, most_common_bit, i)
-    if len(co_list) > 1:
-        co_list = filter_by_bit(co_list, least_common_bit, i)
+    for i in range(1, 12):
+        if len(oxy_gen_rating) > 1:
+            oxy_gen_rating = most_and_least_common_bit_lists(oxy_gen_rating, i)['most common']
+        if len(co2_scrub_rating) > 1:
+            co2_scrub_rating = most_and_least_common_bit_lists(co2_scrub_rating, i)['least common']
+        
+    return int(oxy_gen_rating[0],2) * int(co2_scrub_rating[0], 2)
 
-print(int(oxy_list[0], 2) * int(co_list[0], 2))
+with open('input.txt') as file: input = file.read().splitlines()
 
-
-
-    
-    
-
-
-
+print(life_support_rating(input))
