19987e755c1b1aab3ab42ad14e2af2d6fc037e6d
Daniel Christo <danielchristo@gmail.com>
2021-12-12 17:19:20 -0500

    Solution for challenge 2 in Go

--- day10/challenge_2.go
+++ day10/challenge_2.go
@@ -5,25 +5,32 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"tools"
 )
 
 var openBracket = map[rune]bool{'{': true, '(': true, '<': true, '[': true}
-var matchingBracket = map[rune]rune{'}': '{', ')': '(', '>': '<', ']': '['}
+var matchingClosingBracket = map[rune]rune{'}': '{', ')': '(', '>': '<', ']': '['}
+var matchingOpeningBracket = map[rune]rune{'{': '}', '(': ')', '<': '>', '[': ']'}
 var bracketValue = map[rune]int{'}': 1197, ')': 3, '>': 25137, ']': 57}
+var openBracketValue = map[rune]int{'{': 3, '(': 1, '<': 4, '[': 2}
+var incompleteSums []int
 
 func main() {
-
 	fileLines := load_input("input.txt")
 	curruptScore := 0
 	for _, line := range fileLines {
 		isCurrupt, curruptCharacter := check_chuncks(line)
 		if isCurrupt {
 			curruptScore += bracketValue[curruptCharacter]
+		} else {
+			incompleteSums = append(incompleteSums, check_incomplete(line))
 		}
 	}
-	fmt.Println(curruptScore)
 
+	midPoint := len(incompleteSums) / 2
+	sort.Ints(incompleteSums)
+	fmt.Println(incompleteSums[midPoint])
 }
 
 func load_input(fileName string) (fileLines []string) {
@@ -44,6 +51,33 @@ func load_input(fileName string) (fileLines []string) {
 	return lines
 }
 
+func check_incomplete(line string) int {
+	var stack tools.Stack
+	var incompleteSum = 0
+	for _, character := range line {
+		if openBracket[character] {
+			stack.Push(character)
+		} else {
+			if peekChar, exists := stack.Peek(); exists {
+				if expectedOpeningBracket, exists := matchingClosingBracket[character]; exists {
+					if peekChar == expectedOpeningBracket {
+						stack.Pop()
+					}
+				}
+			}
+		}
+	}
+
+	for exists, popChar := true, ' '; exists; popChar, exists = stack.Pop() {
+		if exists && popChar != ' ' {
+			incompleteSum *= 5
+			incompleteSum += openBracketValue[popChar]
+		}
+	}
+
+	return incompleteSum
+}
+
 func check_chuncks(line string) (bool, rune) {
 	var stack tools.Stack
 	for _, character := range line {
@@ -51,7 +85,7 @@ func check_chuncks(line string) (bool, rune) {
 			stack.Push(character)
 		} else {
 			if peekChar, exists := stack.Peek(); exists {
-				if expectedOpeningBracket, exists := matchingBracket[character]; exists {
+				if expectedOpeningBracket, exists := matchingClosingBracket[character]; exists {
 					if peekChar == expectedOpeningBracket {
 						stack.Pop()
 					} else {
