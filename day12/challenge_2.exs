e7f72596f8f9e591e1858b4c21e3d0a9ddcaa4fb
Daniel Christo <daniel.c@thinkific.com>
2021-12-16 21:59:18 -0500

    Day 12 Challenge 1 in Elixir

---
+++ day12/challenge_2.exs
@@ -0,0 +1,52 @@
+defmodule Challenge1 do
+  def traverseCaves(currentCave, pathMap, result, visited) do
+    if currentCave == "end" do
+      (result <> ", " <> currentCave)
+    else
+      if !MapSet.member?(visited, currentCave) do
+        result =
+          if currentCave != "start" do
+            result <> ", " <> currentCave
+          else
+            result <> currentCave
+          end
+
+        visited =
+          if String.downcase(currentCave) == currentCave do
+            MapSet.put(visited, currentCave)
+          else
+            visited
+          end
+        for nextCave <- pathMap[currentCave] do
+          traverseCaves(nextCave, pathMap, result, visited)
+        end
+      end
+    end
+  end
+end
+
+resolveMapMergeConflicts =
+  fn _k, list1, list2 ->
+    list1 ++ list2
+  end
+reducePathMaps =
+  fn array, accumulator ->
+    Map.merge(accumulator, array, resolveMapMergeConflicts)
+  end
+createPathMaps =
+  fn array ->
+    [location1,location2] = array
+    %{location1 => [location2], location2 => [location1]}
+  end
+
+inputPathMap = "input.txt"
+        |> File.stream!
+        |> Enum.map(&String.trim/1)
+        |> Enum.map(&String.split(&1, "-"))
+        |> Enum.map(createPathMaps)
+        |> Enum.reduce(%{}, reducePathMaps)
+
+traversalList = Challenge1.traverseCaves("start", inputPathMap, "", MapSet.new())
+resultList = Enum.filter(List.flatten(traversalList), &!is_nil(&1))
+IO.inspect resultList
+IO.inspect length(resultList)
