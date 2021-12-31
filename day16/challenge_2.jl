2c0c9c28fe145dc41894659393450a2eac761bc7
Daniel Christo <danielchristo@gmail.com>
2021-12-31 08:25:25 -0500

    Day 16 Challenge 2 in Julia

--- day16/challenge_2.jl
+++ day16/challenge_2.jl
@@ -1,23 +1,54 @@
 include("bits_parser.jl")
-using BitsParser
+include("converter.jl")
+using BitsParser, Converter
 
-function hexDigitToFourBinaryDigits(hexValue)
-    lpad(bin(parse(Int16, hexValue, 16)), 4, "0")
-end
-
-function convertFileContentToBinary(fileName)
-    file = open(fileName,"r")
-    input = ""
-    while !eof(file)
-        hexChar = read(file, Char)
-        binaryString = hexDigitToFourBinaryDigits(hexChar)
-        input = string(input, binaryString)    
+function evalGreaterThan(arg1,arg2)
+    if arg1 > arg2
+        1
+    else
+        0
     end
-    close(file)
-    input
 end
 
-input = convertFileContentToBinary("input_test8.txt")
-result = processBits(input)
-printPackets(result, 1)
+function evalLessThan(arg1,arg2)
+    if arg1 < arg2
+        1
+    else
+        0
+    end
+end
 
+function evalEqualTo(arg1,arg2)
+    if arg1 == arg2
+        1
+    else
+        0
+    end
+end
+
+
+evalFunctionMap = Dict(0 => +, 1 => *, 2 => min, 3 => max, 5 => evalGreaterThan, 6 => evalLessThan, 7 => evalEqualTo)
+
+function evaluatePacket(packet)
+    typeVal = packet["Type"]
+    
+    if typeVal == 4
+        return packet["LiteralVal"]
+    end
+
+    args = []
+    for subPacket in packet["Contains"]
+        push!(args, evaluatePacket(subPacket))
+    end
+
+    evalFunc = evalFunctionMap[typeVal]
+    result = reduce(evalFunctionMap[typeVal], args)
+    result    
+end
+
+
+input = convertFileContentToBinary("input.txt")
+result = parseBits(input)
+resultEval = evaluatePacket(result)
+printPackets(result, 1)
+println(resultEval)
