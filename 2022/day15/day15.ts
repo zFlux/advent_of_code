6a10a8e9524d83aa64633134c6acb4a761002b24
Daniel Christo <daniel.c@thinkific.com>
2022-12-28 08:26:25 +0530

    feat(aoc): Day 15 part 2

--- 2022/day15/day15.ts
+++ 2022/day15/day15.ts
@@ -4,10 +4,14 @@ const findDistance = (x1:number, y1:number, x2:number, y2:number) => {
     return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 }
 
+const findRangeSize = (r: Range) => {
+    return Math.abs(r.x2 - r.x1);
+}
+
 class Sensor {
     x: number;
     y: number;
-    closestBeacon: {x: number, y: number, distance?: number};
+    closestBeacon: {x: number, y: number, distance: number};
     constructor(x: number, y: number, beaconX: number, beaconY: number) {
         this.x = x;
         this.y = y;
@@ -15,10 +19,19 @@ class Sensor {
     }
 }
 
+class Range {
+    x1: number;
+    x2: number;
+    constructor(x1: number, x2: number) {
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+}
+
 const sensors: Sensor[] = [];
 const input = fs.readFileSync('input.txt', 'utf8').split('\n');
 input.forEach(line => {
-    const matches = line.match(/Sensor at x=(\d+), y=(\d+): closest beacon is at x=(\d+), y=(\d+)/);
+    const matches = line.match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/);
     if (matches) {
         sensors.push(
             new Sensor(parseInt(matches[1]), parseInt(matches[2]), 
@@ -26,34 +39,87 @@ input.forEach(line => {
         }
 });
 
-const findBeaconlessCoordsAlongY = (sensor: Sensor, y:number, beaconLess: Set<string>,  knownBeacons: Set<string> ): Set<string> => {
-    // find the distance from the query y to the sensor y
+const findBeaconlessRange = (sensor: Sensor, y:number): Range | undefined => {
     const queryDist = Math.abs(y - sensor.y);
-    // if the query distance is greater than the closest beacon distance
-    // then no point can be ruled out as beaconless
-    if (queryDist > sensor.closestBeacon.distance) return beaconLess;
-    // if it's equal only the point at sensor's x is beaconless
-    // except if it's a known beacon
-    const point = sensor.x.toString() + ',' + y.toString()
-    if(!knownBeacons.has(point)) beaconLess.add(point);
-    for (let i = 1; i <= sensor.closestBeacon.distance - queryDist; i++) {
-        const pointPlus = (sensor.x + i).toString() + ',' + y.toString();
-        const pointMinus = (sensor.x - i).toString() + ',' + y.toString();
-        if(!knownBeacons.has(pointPlus)) beaconLess.add(pointPlus);
-        if(!knownBeacons.has(pointMinus)) beaconLess.add(pointMinus);
-    }
-
-    return beaconLess;
+    if (queryDist > sensor.closestBeacon.distance) return undefined;
+    const manhattanX = sensor.closestBeacon.distance - queryDist;
+    return new Range(sensor.x - manhattanX, sensor.x + manhattanX);
 }
 
-let result: Set<string> = new Set<string>();
 let knownBeacons: Set<string> = new Set<string>();
 sensors.forEach( sensor => {
-    knownBeacons.add(sensor.closestBeacon.x.toString() + ',' + sensor.closestBeacon.y.toString());
-})
-sensors.forEach( sensor => {
-    result = findBeaconlessCoordsAlongY(sensor, 2000000, result, knownBeacons);
+    knownBeacons.add(`${sensor.closestBeacon.x},${sensor.closestBeacon.y}`);
 })
 
-console.log('Part 1: ' + result.size);
+const findBeaconlessRanges = (sensors: Sensor[], y:number): Range[] => {
+    // list of ranges that are beaconless
+    let beaconlessRanges: Range[] = [];
+    sensors.forEach( sensor => {
+        const newRange = findBeaconlessRange(sensor, y);
+        if (newRange) {
+            if (beaconlessRanges.length === 0) beaconlessRanges.push(newRange);
+            else {
+                let newBeaconlessRanges: Range[] = [];
+                let merged = false;
+                for(let i = 0; i < beaconlessRanges.length; i++) {
+                    const range = beaconlessRanges[i];
+                    if (range.x2 < newRange.x1 - 1 || range.x1 > newRange.x2 + 1) {
+                        newBeaconlessRanges.push(range);
+                    } else {
+                        merged = true;
+                        newRange.x1 = Math.min(range.x1, newRange.x1);
+                        newRange.x2 = Math.max(range.x2, newRange.x2);
+                    }                
+                }
+                newBeaconlessRanges.push(newRange);
+                beaconlessRanges = newBeaconlessRanges;
+            }
+        }
+    });
+
+    let resultSum = 0;
+    beaconlessRanges.forEach( range => {
+        resultSum+= Math.abs(range.x2 - range.x1) + 1;
+        knownBeacons.forEach( beacon => {
+            const beaconX = parseInt(beacon.split(',')[0]);
+            const beaconY = parseInt(beacon.split(',')[1]);
+            if (beaconX >= range.x1 && beaconX <= range.x2 && beaconY === y) resultSum--;
+        });
+    });
+
+    return beaconlessRanges;
+};
+
+const ranges = findBeaconlessRanges(sensors, 10);
+console.log(`Part 1: ${findRangeSize(ranges[0])}`);
+
+// Part 2
+const findBeaconRanges = (beaconless: Range[], searchArea: Range, row:number, result: {x: number, y: number}) => {
+    // if the beaconless ranges are greater than 1 then find the ranges that are between 
+    // beaconless ranges
+    if (beaconless.length > 1) {
+        for(let i = 0; i < beaconless.length - 1; i++) {
+            const range1 = beaconless[i];
+            const range2 = beaconless[i+1];
+            if (range1.x2 < range2.x1 - 1) {
+                const newRange = new Range(range1.x2 + 1, range2.x1 - 1);
+                if (newRange.x1 >= searchArea.x1 && newRange.x2 <= searchArea.x2) {
+                    result.x = newRange.x1, 
+                    result.y = row;
+                    return;
+                }
+            }
+        }
+    } 
+    return result;
+}
+
+let beaconRanges: {x: number, y: number} = {x: 0, y: 0};
+for (let i = 0; i < 4_000_000; i++) {
+    const beaconlessRanges = findBeaconlessRanges(sensors, i);
+    if (beaconlessRanges.length > 1) {
+        findBeaconRanges(beaconlessRanges, new Range(0, 4_000_000), i, beaconRanges);
+    }
+}
+console.log(`Part 2: ${beaconRanges.x * 4_000_000 + beaconRanges.y}`);
 
