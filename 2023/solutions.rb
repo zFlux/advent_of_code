d163c89359768c32fe0f6614b0492cf25d18979b
Daniel Christo <daniel.c@thinkific.com>
2023-12-02 21:28:19 -0500

    feat(2023_aoc): Day solutions

--- 2023/solutions.rb
+++ 2023/solutions.rb
@@ -1,5 +1,13 @@
 require_relative 'day1/challenge_one'
+require_relative 'day2/challenge_two'
 
-puts "Day 1 Challenge"
-puts "--------------"
-ChallengeOne.solution
+challenges = [ChallengeOne, ChallengeTwo]
+
+challenges.each_with_index.map do |challenge, index|
+    puts ""
+    puts challenge.name
+    puts "--------------"
+    challenge.solutions("day#{index + 1}/input.txt").each_with_index do |solution, index|
+        puts "Part #{index + 1}: #{solution}"
+    end
+end
