bf27325629f6f2317f1bbe3b0bc20c2218e8ed96
Daniel Christo <daniel.c@thinkific.com>
2023-12-03 20:31:36 -0500

    feat(2023_aoc): Add solution for part 2 of day 3 2023 aoc

--- 2023/day3/challenge_three.rb
+++ 2023/day3/challenge_three.rb
@@ -5,8 +5,17 @@ class ChallengeThree
     class << self
         def solutions(input_file_path)
             engine_map = InputFileReader.read_file_to_list(input_file_path)
-            part_numbers = EngineMap.find_part_numbers(engine_map)
-            part_numbers.sum
+            parts_and_gear_map = EngineMap.find_part_numbers(engine_map)
+            parts = parts_and_gear_map[0]
+            gear_map = parts_and_gear_map[1]
+            
+            # filter for gear maps with a list of size two
+            # and then map to the product of the two numbers
+            gear_map.select! { |k, v| v.size == 2 }
+            gear_map = gear_map.map { |k, v| v.reduce(:*) }
+
+            # return the sum of the parts and the sum of the gear map
+            [parts, gear_map.sum]
         end
     end
 end
@@ -14,6 +23,8 @@ end
 class EngineMap
     class << self
         
+        GEAR_MAP = {}
+
         def find_part_numbers(engine_map)
             part_numbers = []
             engine_map.each_with_index.map do |line, row|
@@ -43,7 +54,17 @@ class EngineMap
                     end
                 end
             end
-            part_numbers
+            [part_numbers.sum, GEAR_MAP]
+        end
+
+        def update_gear_map(map, row, col, number)
+            if map[row][col] == '*'
+                if GEAR_MAP["#{row},#{col}"].nil?
+                    GEAR_MAP["#{row},#{col}"] = [number.to_i]
+                else
+                    GEAR_MAP["#{row},#{col}"] << number.to_i
+                end
+            end
         end
 
         def adjacent_special_chars?(map, number, row, col)
@@ -62,10 +83,22 @@ class EngineMap
                 # if the current col is equal to first or last col 
                 # then check all three rows
                 if curr_col == top_left_col || curr_col == col
-                    has_special_char = has_special_char || is_special_char_at?(map, curr_row, curr_col) || is_special_char_at?(map, curr_row + 1, curr_col) || is_special_char_at?(map, curr_row + 2, curr_col)
+                    0.upto(2) do |i|
+                        if is_special_char_at?(map, curr_row + i, curr_col)
+                            has_special_char = true
+                            update_gear_map(map, curr_row + i, curr_col, number)
+                        end
+                    end
                 else
                     # else check only the first and last row
-                    has_special_char = has_special_char || is_special_char_at?(map, curr_row, curr_col) || is_special_char_at?(map, curr_row + 2, curr_col)
+                    if is_special_char_at?(map, curr_row, curr_col)
+                        has_special_char = true
+                        update_gear_map(map, curr_row, curr_col, number)
+                    end
+                    if is_special_char_at?(map, curr_row + 2, curr_col)
+                        has_special_char = true
+                        update_gear_map(map, curr_row + 2, curr_col, number)
+                    end
                 end
                 curr_col += 1
             end
@@ -99,5 +132,3 @@ class EngineMap
     end
 
 end
-
-puts ChallengeThree.solutions("input.txt")
