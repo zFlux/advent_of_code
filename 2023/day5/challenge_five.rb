4bf023bc4c65904ba328860577848c34052a398d
Daniel Christo <daniel.c@thinkific.com>
2023-12-07 21:15:01 -0500

    feat(2023_aoc): Day 5 part 2 solution

--- 2023/day5/challenge_five.rb
+++ 2023/day5/challenge_five.rb
@@ -9,12 +9,21 @@ class ChallengeFive
             almanac_strings = input_list[2..-1]
             garden_almanac = GardenAlmanac.new(almanac_strings)
 
+            # Part 1
             location_distances = []
             seeds.map do |seed|
                 location_distances << garden_almanac.find_location_from_seed(seed)
             end
             part_1 = location_distances.min
             part_1
+
+            # Part 2
+            location_ranges = []
+            seed_ranges = ResourceRangeList.new(seeds)
+            location_ranges = garden_almanac.find_location_range_from_seed_ranges(seed_ranges)
+            # find the array with the smallest first element
+            part_2 = location_ranges.min_by(&:range_start).range_start
+            [part_1, part_2]
         end
     end
 end
@@ -37,6 +46,18 @@ class GardenAlmanac
         curr_num
     end
 
+    # Takes a list of seed ranges and returns a list of location ranges
+    def find_location_range_from_seed_ranges(seed_ranges)
+        curr_seed_ranges = seed_ranges.resource_ranges
+        #print "Seed Ranges: #{seed_ranges.inspect}"
+        @resource_maps.each do |resource_map|
+            curr_seed_ranges = resource_map.map_source_resource_ranges_to_dest_ranges(curr_seed_ranges)
+            #puts resource_map.name
+            #puts curr_seed_ranges.inspect
+        end
+        curr_seed_ranges
+    end
+
     def parse_resource_maps(almanac_strings)
         resource_map_name = ''
         resource_map_ranges = []
@@ -45,7 +66,7 @@ class GardenAlmanac
             if almanac_string.include?(':')
                 resource_map_name = almanac_string.split(':')[0]
             elsif almanac_string.match?(/\d/)
-                resource_map_ranges << Range.new(almanac_string.split(' ').map(&:to_i))
+                resource_map_ranges << MapRange.new(almanac_string.split(' ').map(&:to_i))
             else
                 @resource_maps << ResourceMap.new(resource_map_name, resource_map_ranges)
                 resource_map_ranges = []
@@ -71,28 +92,129 @@ class ResourceMap
         end
         source_num
     end
+
+    # Takes a list of resource ranges and returns a list of destination resource ranges
+    def map_source_resource_ranges_to_dest_ranges(ranges)
+        result_arr = []
+        unprocessed = []
+        ranges.each do |range|
+            mapped_parts = []
+            unprocessed = [range]
+            @resource_map_ranges.each do |resource_map_range|
+                unmapped = []
+                unprocessed.each do |unprocessed_range| 
+                    result = resource_map_range.map_range(unprocessed_range)
+                    mapped_parts+=result[:mapped]
+                    unmapped+=result[:unmapped]
+                end
+                unprocessed = unmapped
+            end
+            result_arr += mapped_parts
+            result_arr+=unprocessed
+        end
+        # if none of the maps have mapped the the range then return the original range
+        result_arr
+    end
 end
 
-class Range
-    attr_reader :dest_range_start, :source_range_start, :range_length
+class ResourceRangeList
+    attr_reader :resource_ranges
 
     def initialize(range_list)
-        @dest_range_start = range_list[0] 
-        @source_range_start = range_list[1] 
-        @range_length = range_list[2] 
+        @resource_ranges = []
+        range_list.each_slice(2) do |range_list|
+            @resource_ranges << ResourceRange.new(range_list)
+        end
+    end
+end
+
+class ResourceRange
+    attr_reader :range_start, :range_length
+
+    def initialize(range_list)
+        @range_start = range_list[0]
+        @range_length= range_list[1]
     end
 
     def inspect
-        "[#{@dest_range_start}, #{@source_range_start}, #{@range_length}]"
+        "[#{@range_start}, #{@range_length}]"
+    end
+end
+
+class MapRange
+    attr_reader :dest_range_start, :source_range_start, :range_length
+
+    def initialize(range_list)
+        @dest_map_range_start = range_list[0] 
+        @source_map_range_start = range_list[1] 
+        @range_map_length = range_list[2] 
+    end
+
+    def inspect
+        "[#{@dest_map_range_start}, #{@source_map_range_start}, #{@range_map_length}]"
     end
 
     def map(source_num)
-        if source_num >= @source_range_start && source_num < @source_range_start + @range_length
-            offset = source_num - @source_range_start
-            @dest_range_start + offset
+        if source_num >= @source_map_range_start && source_num < @source_map_range_start + @range_map_length
+            offset = source_num - @source_map_range_start
+            @dest_map_range_start + offset
         else
             nil
         end
+    end
+
+    # returns a list of ranges
+    def map_range(range)
+        if map_range_contains_range(range)
+            # then the entire range is mapped so return the mapped range
+            offset_start = range.range_start - @source_map_range_start
+            dest_start = @dest_map_range_start + offset_start
+            return { mapped: [ResourceRange.new([dest_start, range.range_length])], unmapped: []}
+        elsif range_contains_map_range(range)
+            # then there are three ranges to return
+            # 1. the range before the mapped range
+            first_range = ResourceRange.new([range.range_start, @source_map_range_start - range.range_start])
+            # 2. the mapped range
+            second_range = ResourceRange.new([@dest_map_range_start, @range_map_length])
+            # 3. the range after the mapped range
+            third_range = ResourceRange.new([@source_map_range_start + @range_map_length, (range.range_start + range.range_length) - (@source_map_range_start + @range_map_length) ])
+            return {mapped: [second_range], unmapped: [first_range, third_range]}
+        elsif range_is_partially_mapped_at_the_end(range)
+            # then there are two ranges to return
+            # 1. the range before the mapped range
+            first_range = ResourceRange.new([range.range_start, @source_map_range_start - range.range_start])
+            # 2. the mapped range
+            offset = (range.range_start + range.range_length) - @source_map_range_start 
+            second_range = ResourceRange.new([@dest_map_range_start, offset])
+            return {mapped: [second_range], unmapped: [first_range]}
+        elsif range_is_partially_mapped_at_the_start(range)
+            # then there are two ranges to return
+            # 1. the mapped range
+            offset = @source_map_range_start + @range_map_length - range.range_start
+            first_range = ResourceRange.new([@dest_map_range_start + (range.range_start - @source_map_range_start), offset])
+            # 2. the range after the mapped range
+            second_range = ResourceRange.new([@source_map_range_start + @range_map_length, range.range_length - offset ])
+            return {mapped: [first_range], unmapped: [second_range]}
+        else
+            # then the range is not mapped at all
+            return {mapped: [], unmapped: [range]}
+        end
+    end
+
+    def map_range_contains_range(range)
+        range.range_start >= @source_map_range_start && range.range_start < @source_map_range_start + @range_map_length && range.range_start + range.range_length <= @source_map_range_start + @range_map_length
+    end
+
+    def range_contains_map_range(range)
+        @source_map_range_start > range.range_start && @source_map_range_start < range.range_start + range.range_length && @source_map_range_start + @range_map_length < range.range_start + range.range_length
+    end
+
+    def range_is_partially_mapped_at_the_end(range)
+        range.range_start <= @source_map_range_start && range.range_start + range.range_length > @source_map_range_start && range.range_start + range.range_length <= @source_map_range_start + @range_map_length
+    end
+
+    def range_is_partially_mapped_at_the_start(range)
+        range.range_start >= @source_map_range_start && range.range_start < @source_map_range_start + @range_map_length && range.range_start + range.range_length >= @source_map_range_start + @range_map_length
     end
 end
 
--- 2023/day5/challenge_five_test.rb
+++ 2023/day5/challenge_five_test.rb
@@ -1,5 +1,138 @@
 require 'minitest/autorun'
-require_relative 'challenge_four'
+require_relative 'challenge_five'
 
 class ChallengeFive_Objects < Minitest::Test
+    def test_map_range_when_input_range_is_contained
+        # source points 0-4 maps to 10-14
+        map_range = MapRange.new([10,0,5])
+
+        # resource points 0-4
+        resource_range = ResourceRange.new([0,5])
+
+        result = map_range.map_range(resource_range)
+        mapped = result[:mapped]
+        unmapped = result[:unmapped]
+
+        assert_equal(1, mapped.length)
+        assert_equal(0, unmapped.length)
+        assert_equal([10,5], [mapped[0].range_start, mapped[0].range_length])
+    end
+
+    def test_map_range_when_map_range_is_contained
+        # source points 5-9 maps to 20-24
+        map_range = MapRange.new([20,5,5])
+        # resource points 0-14
+        resource_range = ResourceRange.new([0,15])
+        result = map_range.map_range(resource_range)
+        mapped = result[:mapped]
+        unmapped = result[:unmapped]
+
+        # expected ranges are 0-4, 20-24 (because it mapped), 10-14
+        assert_equal(1, mapped.length)
+        assert_equal(2, unmapped.length)
+        assert_equal([0,5], [unmapped[0].range_start, unmapped[0].range_length])
+        assert_equal([10,5], [unmapped[1].range_start, unmapped[1].range_length])
+        assert_equal([20,5], [mapped[0].range_start, mapped[0].range_length])
+    end
+
+    def test_map_range_when_input_range_starts_before_map_range
+        # source points 5-9 maps to 20-24
+        map_range = MapRange.new([20,5,5])
+
+        # resource points 0-8
+        resource_range = ResourceRange.new([0,9])
+
+        result = map_range.map_range(resource_range)
+        mapped = result[:mapped]
+        unmapped = result[:unmapped]
+
+        # expected ranges are 0-4, 20-23 (because it mapped)
+        assert_equal(1, mapped.length)
+        assert_equal(1, unmapped.length)
+        assert_equal([0,5], [unmapped[0].range_start, unmapped[0].range_length])
+        assert_equal([20,4], [mapped[0].range_start, mapped[0].range_length])
+    end
+
+    def test_map_range_when_input_range_begins_in_and_ends_after_map_range
+        # source points 5-9 maps to 20-24
+        map_range = MapRange.new([20,5,5])
+
+        # resource points 6-17
+        resource_range = ResourceRange.new([6,12])
+
+        result = map_range.map_range(resource_range)
+        mapped = result[:mapped]
+        unmapped = result[:unmapped]
+
+        # expected ranges are 21-24 (because it mapped) and 10-17
+        assert_equal(1, mapped.length)
+        assert_equal(1, unmapped.length)
+        assert_equal([21,4], [mapped[0].range_start, mapped[0].range_length])
+        assert_equal([10,8], [unmapped[0].range_start, unmapped[0].range_length])
+    end
+
+    def test_map_range_when_input_range_begins_in_and_ends_after_map_range_2
+        # source points 0-4 maps to 10-14
+        map_range = MapRange.new([10,0,5])
+
+        # resource points 0-6
+        resource_range = ResourceRange.new([0,7])
+
+        result = map_range.map_range(resource_range)
+        mapped = result[:mapped]
+        unmapped = result[:unmapped]
+
+        # expected ranges are 10-14 (because it mapped) and 5-6 because it didn't
+        assert_equal(1, mapped.length)
+        assert_equal(1, unmapped.length)
+        assert_equal([10,5], [mapped[0].range_start, mapped[0].range_length])
+        assert_equal([5,2], [unmapped[0].range_start, unmapped[0].range_length])
+    end
+
+    def test_map_range_when_input_range_has_no_overlap_with_map_range
+        # source points 5-9 maps to 20-24
+        map_range = MapRange.new([20,5,5])
+
+        # resource points 20-24
+        resource_range = ResourceRange.new([20,5])
+
+        result = map_range.map_range(resource_range)
+        mapped = result[:mapped]
+        unmapped = result[:unmapped]
+
+        # expected range are 20-24
+        assert_equal(1, unmapped.length)
+        assert_equal([20,5], [unmapped[0].range_start, unmapped[0].range_length])
+    end
+
+    class ResourceMapTest < Minitest::Test
+        def setup
+            @resource_map_ranges = [
+                # source points 6-9 map to 20-23
+                MapRange.new([20,6,4]),
+                # source points 0-4 map to 10-14
+                MapRange.new([10,0,5])
+            ]
+            @resource_map = ResourceMap.new('Test Map', @resource_map_ranges)
+        end
+
+        def test_map_source_resource_for_full_range
+            resource_range_list = ResourceRangeList.new([0,12])
+            # expect [10,5] (0-4), [5,1] (5), [20,4] (6-9), [10,2] (10-11)
+            result = @resource_map.map_source_resource_ranges_to_dest_ranges(resource_range_list.resource_ranges)
+            assert_equal(4, result.length)
+            # assert that the result contains the list [20,4] somewhere inside
+            assert(result.any? {|range| range.range_start == 10 && range.range_length == 5})
+            assert(result.any? {|range| range.range_start == 5 && range.range_length == 1})
+            assert(result.any? {|range| range.range_start == 20 && range.range_length == 4})
+            assert(result.any? {|range| range.range_start == 10 && range.range_length == 2})
+        end
+
+        def test_map_source_resource_for_full_range_2
+            resource_range_list = ResourceRangeList.new([0,12,30,14])
+            # expect [10,5] (0-4), [5,1] (5), [20,4] (6-9), [10,2] (10-11)
+            result = @resource_map.map_source_resource_ranges_to_dest_ranges(resource_range_list.resource_ranges)
+            byebug
+        end
+    end
 end
