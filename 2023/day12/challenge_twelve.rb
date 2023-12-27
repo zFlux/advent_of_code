9a3ca4ea218a90454912715a21367ee766d60169
Daniel Christo <daniel.c@thinkific.com>
2023-12-27 14:29:32 -0500

    fix(2023_aoc): Remove attempt to combine part of a previous solution

--- 2023/day12/challenge_twelve.rb
+++ 2023/day12/challenge_twelve.rb
@@ -16,7 +16,7 @@ class ChallengeTwelve
 end
 
 class SpringGroup
-    attr_reader :condition_records, :count_records, :start_points
+    attr_reader :condition_records, :count_records, :group_start_positions
 
     OPERATIONAL = '.'
     DAMAGED = '#'
@@ -31,10 +31,11 @@ class SpringGroup
             @condition_records += ("?" + @condition_records) * 4
             @count_records += @count_records * 4
         end
+        @group_start_positions = []
         @acc = []
-        @cached_counts = {}
+        @counts = {}
         find_first_group_positions
-        @start_points = @acc.reverse
+        @group_start_positions = @acc.reverse
     end
 
     def find_first_group_positions(search_index = 0, group_index = 0)
@@ -81,73 +82,6 @@ class SpringGroup
             end
         end
         string[group_size] != DAMAGED
-    end
-
-
-    def find_possibilities
-        # going backwards through the start points,
-        # compute the count of possibilities from a given point
-        # and store it in the cache
-        byebug
-        (@start_points.length - 1).downto(0).each do |group_index|
-            group_starts = @start_points[group_index]
-            # take the groups first start point
-            group_start = group_starts[0]
-            # take the groups size
-            group_size = @count_records[group_index]
-            # find all remaining start points
-            start = group_start + 1
-            last = 0
-            if group_index == @start_points.length - 1
-                # if this is the first group (last group) then the last possible start point is the last index minus the group size
-                last = @condition_records.length - group_size
-            else
-                # otherwise the last possible start point is the last start point of the previous group minus the group size
-                previous_groups_starts = @start_points[group_index + 1]
-                previous_groups_last_start = previous_groups_starts[previous_groups_starts.length - 1]
-                last = previous_groups_last_start - group_size - 1
-            end
-
-            (start..last).each do |index|
-                new_start = find_damaged_group_at(index, group_size)
-                if new_start
-                    group_starts << new_start
-                end
-            end
-        end
-
-        # now that we have all the start points, we can compute the number of possibilities
-        # starting with the final group
-
-        # for each of the starting points in the final group store a value in the hash that associates
-        # that group and start point with the possibility count of 1
-        @start_points[@start_points.length - 1].each do |start_point|
-            @cached_counts[[@start_points.length - 1, start_point]] = 1
-        end
-
-        # for the counts of all the other groups, starting with the second to last group
-        # and going backwards, compute the count of possibilities for each start point
-        # and store it in the hash
-        (@start_points.length - 2).downto(0).each do |group_index|
-            group_starts = @start_points[group_index]
-            group_starts.each do |start_point|
-                count = 0
-                next_group_starts = @start_points[group_index + 1]
-                next_group_starts.each do |next_start_point|
-                    if next_start_point > start_point + @count_records[group_index]
-                        count += @cached_counts[[group_index + 1, next_start_point]]
-                    end
-                end
-                @cached_counts[[group_index, start_point]] = count
-            end
-        end
-
-        # possibilities are equal to the sum of possibilities for all possible start positions of the first group
-        possibilities = 0
-        @start_points[0].each do |start_point|
-            possibilities += @cached_counts[[0, start_point]]
-        end
-        possibilities
     end
 end
 
--- 2023/day12/challenge_twelve_test.rb
+++ 2023/day12/challenge_twelve_test.rb
@@ -21,7 +21,7 @@ class ChallengeTwelve_Objects < Minitest::Test
     def test_find_first_group_positions
         input = "?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1"
         spring_group = SpringGroup.new(input)
-        assert_equal [1, 5, 8, 14, 18, 21, 27, 31, 34, 40, 44, 47, 53, 57, 60], spring_group.start_points
+        assert_equal [1, 5, 8, 14, 18, 21, 27, 31, 34, 40, 44, 47, 53, 57, 60], spring_group.group_start_positions
     end
 
     # Do this brute force:
@@ -38,44 +38,38 @@ class ChallengeTwelve_Objects < Minitest::Test
     def test_find_first_group_positions_2
         input = "?###???????? 3,2,1"
         spring_group = SpringGroup.new(input, true)
-        assert_equal [1, 5, 8, 14, 18, 21, 27, 31, 34, 40, 44, 47, 53, 57, 60], spring_group.start_points
+        assert_equal [1, 5, 8, 14, 18, 21, 27, 31, 34, 40, 44, 47, 53, 57, 60], spring_group.group_start_positions
     end
 
     def test_find_first_group_positions_3
         input = "???.### 1,1,3"
         spring_group = SpringGroup.new(input)
-        assert_equal [0, 2, 4], spring_group.start_points
+        assert_equal [0, 2, 4], spring_group.group_start_positions
     end
 
     def test_find_first_group_positions_4
         input = ".??..??...?##. 1,1,3"
         spring_group = SpringGroup.new(input)
-        assert_equal [1, 5, 10], spring_group.start_points
+        assert_equal [1, 5, 10], spring_group.group_start_positions
     end
 
     def test_find_first_group_positions_5
         input = "?#?#?#?#?#?#?#? 1,3,1,6"
         #        .#.###.#.######
         spring_group = SpringGroup.new(input)
-        assert_equal [1, 3, 7, 9], spring_group.start_points
+        assert_equal [1, 3, 7, 9], spring_group.group_start_positions
     end
 
     def test_find_first_group_positions_6
         input = "????.#...#... 4,1,1"
         spring_group = SpringGroup.new(input)
-        assert_equal [0, 5, 9], spring_group.start_points
+        assert_equal [0, 5, 9], spring_group.group_start_positions
     end
 
     def test_find_first_group_positions_7
         input = "????.######..#####. 1,6,5"
         spring_group = SpringGroup.new(input)
-        assert_equal [0, 5, 13], spring_group.start_points
+        assert_equal [0, 5, 13], spring_group.group_start_positions
     end
     
-    def test_find_possibilities
-        input = "?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1"
-        spring_group = SpringGroup.new(input)
-        assert_equal 1, spring_group.find_possibilities
-    end
-
 end
