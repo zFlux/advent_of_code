56a4d29478e04cf2ee8918efb538d7cd36434ad9
Daniel Christo <daniel.c@thinkific.com>
2023-12-27 14:10:50 -0500

    feat(2023_aoc): Day 12 part 2 attempt 3

--- 2023/day12/challenge_twelve.rb
+++ 2023/day12/challenge_twelve.rb
@@ -5,26 +5,18 @@ class ChallengeTwelve
     class << self
         def solutions(input_file_path)
             input_list = InputFileReader.read_file_to_list(input_file_path)
-            # part1_possibility_counts = []
-            # input_list.each do |line|
-            #     part1_spring_group = SpringGroup.new(line, true)
-            #     part1_arrangements = part1_spring_group.count_possible_arrangements
-            #     part1_possibility_counts << part1_arrangements
-            # end
-
+            spring_groups = []
+            input_list.each do |line|
+                spring_group = SpringGroup.new(line, true)
+                spring_groups << spring_group
+            end
             [0, 0]
-
-            # part2_arrangements = []
-            # part2_spring_groups.each do |spring_group|
-            #     part2_arrangements << spring_group.arrangements.size
-            # end
-            # part2_arrangements.sum
         end
     end
 end
 
 class SpringGroup
-    attr_reader :condition_records, :contiguous_groups_of_damaged_springs
+    attr_reader :condition_records, :count_records, :group_start_positions
 
     OPERATIONAL = '.'
     DAMAGED = '#'
@@ -33,118 +25,64 @@ class SpringGroup
     def initialize(input, unfolded = false)
         split_input = input.split(' ')
         @condition_records = split_input[0]
-        @contiguous_groups_of_damaged_springs = split_input[1].split(',')
-        @contiguous_groups_of_damaged_springs.map!(&:to_i)
+        @count_records = split_input[1].split(',')
+        @count_records.map!(&:to_i)
         if unfolded
-            # add five copies of the conditions to the end of the conditions
             @condition_records += ("?" + @condition_records) * 4
-            @contiguous_groups_of_damaged_springs += @contiguous_groups_of_damaged_springs * 4
+            @count_records += @count_records * 4
+        end
+        @group_start_positions = []
+        @acc = []
+        @counts = {}
+        find_first_group_positions
+        @group_start_positions = @acc.reverse
+    end
+
+    def find_first_group_positions(search_index = 0, group_index = 0)
+        if group_index >= @count_records.length
+            return condition_records_above_are_not_damaged?(search_index)
+        end
+
+        if search_index >= @condition_records.length && group_index < @count_records.length
+            return false
+        end
+
+        group_size = @count_records[group_index]
+        slice_to_check = @condition_records[search_index..(search_index + group_size)]
+        #puts "Group Index: #{group_index} Group Size: #{group_size} Slice to check: #{slice_to_check} can contain group: #{can_contain_group(slice_to_check, group_size)}"
+
+        # if this slice can contain the current group
+        if can_contain_group(slice_to_check, group_size)
+            # and the subsequent groups can be found in the remaining string
+            if find_first_group_positions(search_index + group_size + 1, group_index + 1)
+                # then we've found a valid group
+                @acc << search_index
+                return true
+            elsif @condition_records[search_index] == UNKNOWN || @condition_records[search_index] == OPERATIONAL
+                find_first_group_positions(search_index + 1, group_index)
+            else
+                false
+            end
+        # if we're not moving past a damaged section
+        elsif @condition_records[search_index] == UNKNOWN || @condition_records[search_index] == OPERATIONAL
+            find_first_group_positions(search_index + 1, group_index)
+        else
+            false
         end
     end
 
-    def count_possible_arrangements(possibility_node = nil)
-        # first we need to create the initial possibility node
-        # then we recursively create possibility nodes until we reach the level that is the same 
-        # depth as the length of the spring group. We count how many branches make it to that level
-        # and that is the number of possible arrangements
-        #puts possibility_node.to_s if possibility_node
+    def condition_records_above_are_not_damaged?(index)
+        !@condition_records[index..-1] || @condition_records[index..-1].split('').all? { |char| char == UNKNOWN || char == OPERATIONAL }
+    end
 
-        level = 0
-        damaged_group_index = 0
-        damaged_counter = 0
-
-        if !possibility_node.nil?
-            level = possibility_node.level
-            damaged_group_index = possibility_node.damaged_group_index
-            damaged_counter = possibility_node.damaged_counter
-        end
-
-        if level == @condition_records.length && damaged_group_index >= @contiguous_groups_of_damaged_springs.length - 1 && (@contiguous_groups_of_damaged_springs[damaged_group_index].nil? || damaged_counter >= @contiguous_groups_of_damaged_springs[damaged_group_index])
-            return 1
-        else
-            if @condition_records[level] == OPERATIONAL && can_add_operational?(level, damaged_group_index, damaged_counter)
-                if last_damaged_encountered?(damaged_group_index, damaged_counter)
-                    return count_possible_arrangements(PossibilityNode.new(OPERATIONAL, possibility_node, damaged_group_index + 1, 0, level + 1))
-                else
-                    return count_possible_arrangements(PossibilityNode.new(OPERATIONAL, possibility_node, damaged_group_index, damaged_counter, level + 1))
-                end
-            elsif @condition_records[level] == DAMAGED && can_add_damaged?(level, damaged_group_index, damaged_counter)
-                return count_possible_arrangements(PossibilityNode.new(DAMAGED, possibility_node, damaged_group_index, damaged_counter + 1, level + 1))
-            elsif @condition_records[level] == UNKNOWN
-                ops = 0
-                dams = 0
-                if can_add_operational?(level, damaged_group_index, damaged_counter)
-                    if last_damaged_encountered?(damaged_group_index, damaged_counter)
-                        ops = count_possible_arrangements(PossibilityNode.new(OPERATIONAL, possibility_node, damaged_group_index + 1, 0, level + 1))
-                    else
-                        ops = count_possible_arrangements(PossibilityNode.new(OPERATIONAL, possibility_node, damaged_group_index, damaged_counter, level + 1))
-                    end
-                end
-                if can_add_damaged?(level, damaged_group_index, damaged_counter)
-                   dams= count_possible_arrangements(PossibilityNode.new(DAMAGED, possibility_node, damaged_group_index, damaged_counter + 1, level + 1))
-                end
-                return ops + dams
+    def can_contain_group(string, group_size)
+        group_size.times do |index|
+            if string[index] == OPERATIONAL
+                return false
             end
         end
-        return 0
-    end
-
-    def can_add_damaged?(level, damaged_group_index, damaged_counter)
-        expected_damaged = @contiguous_groups_of_damaged_springs[damaged_group_index]
-        expected_damaged && damaged_counter < expected_damaged
-    end
-
-    def can_add_operational?(level, damaged_group_index, damaged_counter)
-        return true if no_damaged_encountered?(damaged_counter) && (enough_room_for_remaining_damaged?(level, damaged_group_index, damaged_counter) || all_damaged_encountered?(damaged_group_index, damaged_counter))
-        return true if last_damaged_encountered?(damaged_group_index, damaged_counter)
-        return false
-    end
-
-    def not_the_last_spot?(level)
-        level < @condition_records.length - 1
-    end
-
-    def enough_room_for_remaining_damaged?(level, damaged_group_index, damaged_counter)
-        remaining_damaged = @contiguous_groups_of_damaged_springs[damaged_group_index] ? @contiguous_groups_of_damaged_springs[damaged_group_index] - damaged_counter : 0
-        remaining_damaged += @contiguous_groups_of_damaged_springs[damaged_group_index + 1..-1] ? @contiguous_groups_of_damaged_springs[damaged_group_index + 1..-1].sum : 0
-        remaining_room = @condition_records.length - level
-        
-        remaining_groups = (damaged_group_index + 1..-1).size
-        remaining_groups = remaining_groups - 1
-
-        remaining_damaged + remaining_groups <= remaining_room
-    end
-    
-    def all_damaged_encountered?(damaged_group_index, damaged_counter)
-        damaged_group_index == @contiguous_groups_of_damaged_springs.length
-    end
-
-    def last_damaged_encountered?(damaged_group_index, damaged_counter)
-        expected_damaged = @contiguous_groups_of_damaged_springs[damaged_group_index]
-        expected_damaged && damaged_counter == expected_damaged
-    end
-
-    def no_damaged_encountered?(damaged_counter)
-        damaged_counter == 0    
+        string[group_size] != DAMAGED
     end
 end
 
-class PossibilityNode
-    attr_reader :value, :parent, :damaged_group_index, :damaged_counter, :level 
-    def initialize(value, parent, damaged_group_index, damaged_counter, level)
-        @value = value
-        @parent = parent
-        @damaged_group_index = damaged_group_index
-        @damaged_counter = damaged_counter
-        @level = level
-    end
-
-    def to_s
-        # recurse up the tree and print the values
-        if @parent.nil?
-            return @value
-        else
-            return @parent.to_s + @value
-        end
-    end
-end
+#puts ChallengeTwelve.solutions('input_test_part1.txt')
--- 2023/day12/challenge_twelve_test.rb
+++ 2023/day12/challenge_twelve_test.rb
@@ -3,31 +3,73 @@ require_relative 'challenge_twelve'
 
 class ChallengeTwelve_Objects < Minitest::Test
 
-    def setup
-        @spring_group = SpringGroup.new("???.### 1,1,3")
-        @spring_group2 = SpringGroup.new("?###???????? 3,2,1")
-        @spring_group3 = SpringGroup.new("?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1")
-        @spring_group4 = SpringGroup.new("???????????? 3,2,1")
-        @spring_group5 = SpringGroup.new("#?????????#?????#?????????#?????#?????????#?????#?????????#?????#?????????#???? 1,3,2,2,1,3,2,2,1,3,2,2,1,3,2,2,1,3,2,2")
+    # test initializing a SpringGroup
+    def test_initialize_spring_group
+        input = "?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1"
+        spring_group = SpringGroup.new(input)
+        assert_equal "?###??????????###??????????###??????????###??????????###????????", spring_group.condition_records
+        assert_equal [3,2,1,3,2,1,3,2,1,3,2,1,3,2,1], spring_group.count_records
     end
 
-    # def test_count_possible_arrangements
-    #     arrangements = @spring_group.count_possible_arrangements
-    #     assert_equal(1, arrangements)
-    # end
+    # ?###??????????###??????????###??????????###??????????###????????
+    #
+    # expected first positions
+    # .###.##.#.....###.##.#.....###.##.#.....###.##.#.....###.##.#...
+    # 1,5,8,14,18,21,27,31,34,40,44,47,53,57,60
 
-    # def test_count_possible_arrangements_2
-    #     arrangements = @spring_group2.count_possible_arrangements
-    #     assert_equal(10, arrangements)
-    # end
-
-    # def test_count_possible_arrangements_3
-    #     arrangements = @spring_group3.count_possible_arrangements
-    #     assert_equal(506250, arrangements)
-    # end
-
-    def test_count_possible_arrangements_4
-        arrangements = @spring_group4.count_possible_arrangements
-        assert_equal(1, arrangements)
+    # test finding the first group positions
+    def test_find_first_group_positions
+        input = "?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1"
+        spring_group = SpringGroup.new(input)
+        assert_equal [1, 5, 8, 14, 18, 21, 27, 31, 34, 40, 44, 47, 53, 57, 60], spring_group.group_start_positions
     end
+
+    # Do this brute force:
+    # 1) Try the first spring group at the first position and check that conditions are met
+    # i.e. there are 3 wildcards or hashes with a wildcard or dot available for a space
+    #   a) If the conditions are met, recurse on to the next spring group, starting at where
+    #   the first spring group ended
+    #   b) If the conditions are not met then move the first spring group to the next position
+    #   and try again
+    # 2) If you've run out of spring groups to recurse on then simply return true
+    #
+
+    # test various other spring groups
+    def test_find_first_group_positions_2
+        input = "?###???????? 3,2,1"
+        spring_group = SpringGroup.new(input, true)
+        assert_equal [1, 5, 8, 14, 18, 21, 27, 31, 34, 40, 44, 47, 53, 57, 60], spring_group.group_start_positions
+    end
+
+    def test_find_first_group_positions_3
+        input = "???.### 1,1,3"
+        spring_group = SpringGroup.new(input)
+        assert_equal [0, 2, 4], spring_group.group_start_positions
+    end
+
+    def test_find_first_group_positions_4
+        input = ".??..??...?##. 1,1,3"
+        spring_group = SpringGroup.new(input)
+        assert_equal [1, 5, 10], spring_group.group_start_positions
+    end
+
+    def test_find_first_group_positions_5
+        input = "?#?#?#?#?#?#?#? 1,3,1,6"
+        #        .#.###.#.######
+        spring_group = SpringGroup.new(input)
+        assert_equal [1, 3, 7, 9], spring_group.group_start_positions
+    end
+
+    def test_find_first_group_positions_6
+        input = "????.#...#... 4,1,1"
+        spring_group = SpringGroup.new(input)
+        assert_equal [0, 5, 9], spring_group.group_start_positions
+    end
+
+    def test_find_first_group_positions_7
+        input = "????.######..#####. 1,6,5"
+        spring_group = SpringGroup.new(input)
+        assert_equal [0, 5, 13], spring_group.group_start_positions
+    end
+
 end
