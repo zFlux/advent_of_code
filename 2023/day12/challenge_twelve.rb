6d3da257532058ecab58ed966218ae7f6a1395d4
Daniel Christo <daniel.c@thinkific.com>
2023-12-28 00:49:10 -0500

    fix(2023_aoc): All tests passing for day 12 part 1 and 2 but not working

--- 2023/day12/challenge_twelve.rb
+++ 2023/day12/challenge_twelve.rb
@@ -5,18 +5,22 @@ class ChallengeTwelve
     class << self
         def solutions(input_file_path)
             input_list = InputFileReader.read_file_to_list(input_file_path)
-            spring_groups = []
+            spring_groups_part1 = []
+            #spring_groups_part2 = []
             input_list.each do |line|
-                spring_group = SpringGroup.new(line, true)
-                spring_groups << spring_group
+                spring_group_part1 = SpringGroup.new(line)
+               # spring_group_part2 = SpringGroup.new(line, true)
+                spring_groups_part1 << spring_group_part1.count_of_possibilities
+               # spring_groups_part2 << spring_group_part2.count_of_possibilities
             end
-            [0, 0]
+            #byebug
+            [spring_groups_part1.sum, 0]
         end
     end
 end
 
 class SpringGroup
-    attr_reader :condition_records, :count_records, :group_start_positions
+    attr_reader :condition_records, :count_records, :group_start_positions, :counts, :count_of_possibilities
 
     OPERATIONAL = '.'
     DAMAGED = '#'
@@ -36,6 +40,9 @@ class SpringGroup
         @counts = {}
         find_first_group_positions
         @group_start_positions = @acc.reverse
+        @count_of_possibilities = @counts[0].values.sum
+        # sum the counts of the first group
+
     end
 
     def find_first_group_positions(search_index = 0, group_index = 0)
@@ -57,6 +64,53 @@ class SpringGroup
             if find_first_group_positions(search_index + group_size + 1, group_index + 1)
                 # then we've found a valid group
                 @acc << search_index
+
+                # Figuring out the counts starts here
+
+                # if we are on the last group then mark a count of 1 in any location that can fit that group
+                # beyond the search index
+                if group_index == @count_records.length - 1
+                    search_index.upto(@condition_records.length - group_size) do |index|
+                        if index > 0 && condition_records[index - 1] == DAMAGED
+                            break
+                        end
+                        if can_contain_group(@condition_records[index..(index + group_size)], group_size)
+                            @counts[group_index] ||= {}
+                            @counts[group_index][index] = 1
+                        end
+                    end
+                else
+                    # otherwise sum the counts of the prior group where the indexes come after the end of where
+                    # the current group ends. i.e. if the current group ends at index 7, then sum the counts from 
+                    # index 8 to the end of the string. If the current group ends at index 8, then sum the counts
+                    # from index 9 to the end of the string etc.
+                    #
+                    # The loop of on the current group starts at search index and ends at the position before the
+                    # last possible group position of the previous group (i.e. the last possible group position of
+                    # the previous group is the end of the string minus the size of the previous group minus 1)
+
+                   # byebug if group_index == 0
+                    # get the last index of the previous group from @counts
+                    last_index_of_previous_group = @counts[group_index + 1].keys.max
+                    search_index.upto(last_index_of_previous_group) do |index|
+                        if index > 0 && condition_records[index - 1] == DAMAGED
+                            break
+                        end
+                        if can_contain_group(@condition_records[index..(index + group_size)], group_size)
+                            #sum the counts of the prior group as long as the index is after the current index + group size
+                            @counts[group_index + 1].each do |key, value|
+                                if key >= index + group_size + 1
+                                    @counts[group_index] ||= {}
+                                    @counts[group_index][index] = @counts[group_index][index].to_i + value
+                                end
+                            end
+                            
+                        end
+                    end
+
+                    # Figuring out the counts ends here
+                end
+
                 return true
             elsif @condition_records[search_index] == UNKNOWN || @condition_records[search_index] == OPERATIONAL
                 find_first_group_positions(search_index + 1, group_index)
@@ -85,4 +139,4 @@ class SpringGroup
     end
 end
 
-#puts ChallengeTwelve.solutions('input_test_part1.txt')
+puts ChallengeTwelve.solutions('input.txt')
--- 2023/day12/challenge_twelve_test.rb
+++ 2023/day12/challenge_twelve_test.rb
@@ -3,73 +3,183 @@ require_relative 'challenge_twelve'
 
 class ChallengeTwelve_Objects < Minitest::Test
 
-    # test initializing a SpringGroup
-    def test_initialize_spring_group
-        input = "?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1"
-        spring_group = SpringGroup.new(input)
-        assert_equal "?###??????????###??????????###??????????###??????????###????????", spring_group.condition_records
-        assert_equal [3,2,1,3,2,1,3,2,1,3,2,1,3,2,1], spring_group.count_records
-    end
+    # # test initializing a SpringGroup
+    # def test_initialize_spring_group
+    #     input = "?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1"
+    #     spring_group = SpringGroup.new(input)
+    #     assert_equal "?###??????????###??????????###??????????###??????????###????????", spring_group.condition_records
+    #     assert_equal [3,2,1,3,2,1,3,2,1,3,2,1,3,2,1], spring_group.count_records
+    # end
 
-    # ?###??????????###??????????###??????????###??????????###????????
-    #
-    # expected first positions
-    # .###.##.#.....###.##.#.....###.##.#.....###.##.#.....###.##.#...
-    # 1,5,8,14,18,21,27,31,34,40,44,47,53,57,60
+    # # ?###??????????###??????????###??????????###??????????###????????
+    # #
+    # # expected first positions
+    # # .###.##.#.....###.##.#.....###.##.#.....###.##.#.....###.##.#...
+    # # 1,5,8,14,18,21,27,31,34,40,44,47,53,57,60
 
-    # test finding the first group positions
-    def test_find_first_group_positions
-        input = "?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1"
-        spring_group = SpringGroup.new(input)
-        assert_equal [1, 5, 8, 14, 18, 21, 27, 31, 34, 40, 44, 47, 53, 57, 60], spring_group.group_start_positions
-    end
+    # # test finding the first group positions
+    # def test_find_first_group_positions
+    #     input = "?###??????????###??????????###??????????###??????????###???????? 3,2,1,3,2,1,3,2,1,3,2,1,3,2,1"
+    #     spring_group = SpringGroup.new(input)
+    #     assert_equal [1, 5, 8, 14, 18, 21, 27, 31, 34, 40, 44, 47, 53, 57, 60], spring_group.group_start_positions
+    # end
 
-    # Do this brute force:
-    # 1) Try the first spring group at the first position and check that conditions are met
-    # i.e. there are 3 wildcards or hashes with a wildcard or dot available for a space
-    #   a) If the conditions are met, recurse on to the next spring group, starting at where
-    #   the first spring group ended
-    #   b) If the conditions are not met then move the first spring group to the next position
-    #   and try again
-    # 2) If you've run out of spring groups to recurse on then simply return true
-    #
+    # # Do this brute force:
+    # # 1) Try the first spring group at the first position and check that conditions are met
+    # # i.e. there are 3 wildcards or hashes with a wildcard or dot available for a space
+    # #   a) If the conditions are met, recurse on to the next spring group, starting at where
+    # #   the first spring group ended
+    # #   b) If the conditions are not met then move the first spring group to the next position
+    # #   and try again
+    # # 2) If you've run out of spring groups to recurse on then simply return true
+    # #
 
-    # test various other spring groups
-    def test_find_first_group_positions_2
-        input = "?###???????? 3,2,1"
-        spring_group = SpringGroup.new(input, true)
-        assert_equal [1, 5, 8, 14, 18, 21, 27, 31, 34, 40, 44, 47, 53, 57, 60], spring_group.group_start_positions
-    end
+    # # test various other spring groups
+    # def test_find_first_group_positions_2
+    #     input = "?###???????? 3,2,1"
+    #     spring_group = SpringGroup.new(input, true)
+    #     assert_equal [1, 5, 8, 14, 18, 21, 27, 31, 34, 40, 44, 47, 53, 57, 60], spring_group.group_start_positions
+    # end
 
-    def test_find_first_group_positions_3
+    # def test_find_first_group_positions_3
+    #     input = "???.### 1,1,3"
+    #     spring_group = SpringGroup.new(input)
+    #     assert_equal [0, 2, 4], spring_group.group_start_positions
+    # end
+
+    # def test_find_first_group_positions_4
+    #     input = ".??..??...?##. 1,1,3"
+    #     spring_group = SpringGroup.new(input)
+    #     assert_equal [1, 5, 10], spring_group.group_start_positions
+    # end
+
+    # def test_find_first_group_positions_5
+    #     input = "?#?#?#?#?#?#?#? 1,3,1,6"
+    #     #        .#.###.#.######
+    #     spring_group = SpringGroup.new(input)
+    #     assert_equal [1, 3, 7, 9], spring_group.group_start_positions
+    # end
+
+    # def test_find_first_group_positions_6
+    #     input = "????.#...#... 4,1,1"
+    #     spring_group = SpringGroup.new(input)
+    #     assert_equal [0, 5, 9], spring_group.group_start_positions
+    # end
+
+    # def test_find_first_group_positions_7
+    #     input = "????.######..#####. 1,6,5"
+    #     spring_group = SpringGroup.new(input)
+    #     assert_equal [0, 5, 13], spring_group.group_start_positions
+    # end
+
+    def test_find_counts_1
         input = "???.### 1,1,3"
         spring_group = SpringGroup.new(input)
-        assert_equal [0, 2, 4], spring_group.group_start_positions
+        assert_equal 1, spring_group.count_of_possibilities
     end
 
-    def test_find_first_group_positions_4
+    def test_find_counts_2
         input = ".??..??...?##. 1,1,3"
         spring_group = SpringGroup.new(input)
-        assert_equal [1, 5, 10], spring_group.group_start_positions
+        assert_equal 4, spring_group.count_of_possibilities
     end
 
-    def test_find_first_group_positions_5
+    def test_find_counts_3
         input = "?#?#?#?#?#?#?#? 1,3,1,6"
-        #        .#.###.#.######
         spring_group = SpringGroup.new(input)
-        assert_equal [1, 3, 7, 9], spring_group.group_start_positions
+        assert_equal 1, spring_group.count_of_possibilities
     end
 
-    def test_find_first_group_positions_6
+    def test_find_counts_4
         input = "????.#...#... 4,1,1"
         spring_group = SpringGroup.new(input)
-        assert_equal [0, 5, 9], spring_group.group_start_positions
+        assert_equal 1, spring_group.count_of_possibilities
     end
 
-    def test_find_first_group_positions_7
+    def test_find_counts_5
         input = "????.######..#####. 1,6,5"
         spring_group = SpringGroup.new(input)
-        assert_equal [0, 5, 13], spring_group.group_start_positions
+        assert_equal 4, spring_group.count_of_possibilities
     end
-    
+
+    def test_find_counts_6
+        input = "?###???????? 3,2,1"
+        spring_group = SpringGroup.new(input)
+        assert_equal 10, spring_group.count_of_possibilities
+    end
+
+    def test_find_counts_7
+        input = "???????????? 3,2,1"
+        spring_group = SpringGroup.new(input)
+        assert_equal 35, spring_group.count_of_possibilities
+    end
+
+    def test_find_counts_8
+        input = "?.???????.. 4,1"
+        # ?.???????..
+        # ..####.#...
+        # ..####..#..
+        # ...####.#..
+
+        spring_group = SpringGroup.new(input)
+        assert_equal 3, spring_group.count_of_possibilities
+    end
+
+    def test_find_counts_9
+        input = ".????#??##?#.????? 1,3,3,1,1,1"
+        # .????#??##?#.?????
+        # .#.###.###.#.#.#..
+        # .#.###.###.#.#..#.
+        # .#.###.###.#.#...#
+        # .#.###.###.#..#.#.
+        # .#.###.###.#..#..#
+        # .#.###.###.#...#.#
+
+        spring_group = SpringGroup.new(input)
+        assert_equal 6, spring_group.count_of_possibilities
+    end
+
+    def test_find_counts_10
+        input = "?##..??.#.?????? 2,1,1,4"
+        spring_group = SpringGroup.new(input)
+        assert_equal 6, spring_group.count_of_possibilities
+    end
+
+    # Unfolded tests
+
+    def test_find_counts_unfolded_1
+        input = "???.### 1,1,3"
+        spring_group = SpringGroup.new(input, true)
+        assert_equal 1, spring_group.count_of_possibilities
+    end
+
+    def test_find_counts_unfolded_2
+        input = ".??..??...?##. 1,1,3"
+        spring_group = SpringGroup.new(input, true)
+        assert_equal 16384, spring_group.count_of_possibilities
+    end
+
+    def test_find_counts_unfolded_3
+        input = "?#?#?#?#?#?#?#? 1,3,1,6"
+        spring_group = SpringGroup.new(input, true)
+        assert_equal 1, spring_group.count_of_possibilities
+    end
+
+    def test_find_counts_unfolded_4
+        input = "????.#...#... 4,1,1"
+        spring_group = SpringGroup.new(input, true)
+        assert_equal 16, spring_group.count_of_possibilities
+    end
+
+    def test_find_counts__unfolded_5
+        input = "????.######..#####. 1,6,5"
+        spring_group = SpringGroup.new(input, true)
+        assert_equal 2500, spring_group.count_of_possibilities
+    end
+
+    def test_find_counts_unfolded_6
+        input = "?###???????? 3,2,1"
+        spring_group = SpringGroup.new(input, true)
+        assert_equal 506250, spring_group.count_of_possibilities
+    end
 end
